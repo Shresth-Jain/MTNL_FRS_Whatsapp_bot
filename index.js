fc58ee0a589a7c53fec095a2ef92086f81ed7c4a
Shresth Jain <er.shresthjain@gmail.com>
2022-02-16 18:49:44 +0530

    correct currdate methods

--- index.js
+++ index.js
@@ -110,19 +110,19 @@ async function startApp(){
     }   
  }
 
-const date_ob = new Date();
-const currUTCHour=date_ob.getUTCHours();
-const currUTCMin=date_ob.getUTCMinutes();
 
 console.log("Working Hours: 9:30 to 15:00 IST -> 4:00 - 9:30 UTC");
 
     // run this function every 10 minutes = 10*60*1000 milliseconds
 var interval=setInterval(()=>{
+    const date_ob = new Date();
+    const currUTCHour=date_ob.getUTCHours();
+    const currUTCMin=date_ob.getUTCMinutes();
     // if time is between 1000Hours to 1500Hours then run the command at regular interval of 10 minutes
     //  10am to 3pm IST  is 4:30am to 9:30pm UTC
         if((currUTCHour>=4 && currUTCHour<9) || (currUTCHour==9 && currUTCMin<=30)){   
             console.log("["+currUTCHour+":"+currUTCMin+"] "+"Starting the application at: ",currUTCHour);
-            startApp();
+            // startApp();
         }
         else if(landlineSent.size>0){
             console.log("["+currUTCHour+":"+currUTCMin+"] "+"Working Hours are over! Cleaning the memory");
